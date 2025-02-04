6ff1887d8077006a2106cf4e8c82a97777bd52f0
Pedro <phribeiro2001ph@gmail.com>
2025-02-04 19:28:54 -0300

    test

--- simulator.py
+++ simulator.py
@@ -6,9 +6,7 @@
 ##           |         pip install -r requirements.txt          | 
 ##           +--------------------------------------------------+
 
-
-
-
+import json
 import time
 import json
 import os
